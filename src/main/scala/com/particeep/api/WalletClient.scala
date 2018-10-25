32cbc644dde455f7d1ec8d8ec9a272173850020c
yanlin <qu.yanlin>
2018-10-25 11:01:22 +0200

    rajouter une méthode delete

--- src/main/scala/com/particeep/api/WalletClient.scala
+++ src/main/scala/com/particeep/api/WalletClient.scala
@@ -85,12 +85,16 @@ class WalletClient(val ws: WSClient, val credentials: Option[ApiCredential] = No
     ws.put[BankAccount](s"$endPoint/bankaccount/$target_id", timeout, Json.toJson(bankAccountCreation))
   }
 
-  def updateBankAccountOffline(bankaccount_id: String, bank_account_creation: BankAccountCreation, timeout: Long = defaultTimeOut)(implicit exec: ExecutionContext): Future[Either[ErrorResult, BankAccount]] = {
-    ws.post[BankAccount](s"$endPoint/bankaccount/$bankaccount_id", timeout, Json.toJson(bank_account_creation))
+  def updateBankAccountOffline(bankaccount_id: String, bank_account_update: BankAccountCreation, timeout: Long = defaultTimeOut)(implicit exec: ExecutionContext): Future[Either[ErrorResult, BankAccount]] = {
+    ws.post[BankAccount](s"$endPoint/bankaccount/$bankaccount_id", timeout, Json.toJson(bank_account_update))
   }
 
   def searchBankAccounts(criteria: BankAccountSearch, table_criteria: TableSearch, timeout: Long = defaultTimeOut)(implicit exec: ExecutionContext): Future[Either[ErrorResult, PaginatedSequence[BankAccount]]] = {
     ws.get[PaginatedSequence[BankAccount]](s"$endPoint/bankaccount/search", timeout, LangUtils.productToQueryString(criteria) ++ LangUtils.productToQueryString(table_criteria))
+  }
+
+  def deleteBankAccountOffline(id: String, timeout: Long = defaultTimeOut)(implicit exec: ExecutionContext): Future[Either[ErrorResult, BankAccount]] = {
+    ws.delete[BankAccount](s"$endPoint/bankaccount/$id", timeout)
   }
 
   def getBankAccountsByWalletId(id: String, timeout: Long = defaultTimeOut)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Seq[BankAccount]]] = {
