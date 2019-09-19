2e91f8b761a3e5f8910e42e4ba61a23c87ba149b
lasne_noe <noe.lasne@particeep.com>
2019-09-19 11:19:51 +0200

    Add tag on role

--- src/main/scala/com/particeep/api/models/role/RoleCreation.scala
+++ src/main/scala/com/particeep/api/models/role/RoleCreation.scala
@@ -8,7 +8,8 @@ import play.api.libs.json.Json
 
 case class RoleCreation(
   target_id:   Option[String] = None,
-  target_type: Option[String] = None
+  target_type: Option[String] = None,
+  tag:         Option[String] = None
 )
 
 object RoleCreation {
