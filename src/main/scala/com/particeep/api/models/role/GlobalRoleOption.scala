6c85a00a36b8f08fc6b7569f191ef4d209f7571b
quyanlin <qu.yanlin@particeep.com>
2020-04-03 10:23:14 +0200

    passer compile

--- src/main/scala/com/particeep/api/models/role/GlobalRoleOption.scala
+++ src/main/scala/com/particeep/api/models/role/GlobalRoleOption.scala
@@ -3,9 +3,9 @@ package com.particeep.api.models.role
 import play.api.libs.json.Json
 
 case class GlobalRoleOption(
-                             role_name: Option[String] = None,
-                             tag:       Option[String] = None
-                           )
+  role_name: Option[String] = None,
+  tag:       Option[String] = None
+)
 
 object GlobalRoleOption {
   val format = Json.format[GlobalRoleOption]
