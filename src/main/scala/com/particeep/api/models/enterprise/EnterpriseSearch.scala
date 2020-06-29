d4ce4a320fc0de77e589f272f6170cb1ded5e9a1
quyanlin <qu.yanlin@particeep.com>
2020-06-29 10:14:11 +0200

    autoAssigneEnterpriseToPartner

--- src/main/scala/com/particeep/api/models/enterprise/EnterpriseSearch.scala
+++ src/main/scala/com/particeep/api/models/enterprise/EnterpriseSearch.scala
@@ -6,17 +6,17 @@ import java.time.ZonedDateTime
  * Created by Noe on 10/03/2017.
  */
 case class EnterpriseSearch(
-  name:              Option[String]        = None,
-  activity_domains:  Option[String]        = None,
-  legal_status:      Option[String]        = None,
-  description_short: Option[String]        = None,
-  description_long:  Option[String]        = None,
-  city:              Option[String]        = None,
-  ids:               Option[String]        = None,
-  createdAfter:      Option[ZonedDateTime] = None,
-  creationAfter:     Option[ZonedDateTime] = None,
-  statuses:          Option[String]        = None,
-  siret:             Option[String]        = None,
-  tag:               Option[String]        = None,
+  name:                         Option[String]        = None,
+  activity_domains:             Option[String]        = None,
+  legal_status:                 Option[String]        = None,
+  description_short:            Option[String]        = None,
+  description_long:             Option[String]        = None,
+  city:                         Option[String]        = None,
+  ids:                          Option[String]        = None,
+  createdAfter:                 Option[ZonedDateTime] = None,
+  creationAfter:                Option[ZonedDateTime] = None,
+  statuses:                     Option[String]        = None,
+  siret:                        Option[String]        = None,
+  tag:                          Option[String]        = None,
   is_auto_assigned_to_partners: Option[Boolean]       = None
 )
