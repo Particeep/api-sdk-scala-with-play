d98d3a03825a65916ba40da7a9012c896a6f2ef1
quyanlin <qu.yanlin@particeep.com>
2020-06-29 10:11:16 +0200

    autoAssignEnterpriseToPartner

--- src/main/scala/com/particeep/api/models/enterprise/Enterprise.scala
+++ src/main/scala/com/particeep/api/models/enterprise/Enterprise.scala
@@ -7,26 +7,26 @@ import com.particeep.api.models.Address
 import play.api.libs.json.{ JsObject, Json }
 
 case class Enterprise(
-  id:                String                = "",
-  created_at:        Option[ZonedDateTime] = None,
-  creation_date:     Option[ZonedDateTime] = None,
-  name:              String                = "",
-  activity_domain:   Option[String]        = None,
-  legal_status:      Option[String]        = None,
-  description_short: Option[String]        = None,
-  description_long:  Option[String]        = None,
-  share_price:       Option[Int]           = None,
-  url:               Option[String]        = None,
-  logo_url:          Option[String]        = None,
-  image_cover_url:   Option[String]        = None,
-  video_url:         Option[String]        = None,
-  website_url:       Option[String]        = None,
+  id:                           String                = "",
+  created_at:                   Option[ZonedDateTime] = None,
+  creation_date:                Option[ZonedDateTime] = None,
+  name:                         String                = "",
+  activity_domain:              Option[String]        = None,
+  legal_status:                 Option[String]        = None,
+  description_short:            Option[String]        = None,
+  description_long:             Option[String]        = None,
+  share_price:                  Option[Int]           = None,
+  url:                          Option[String]        = None,
+  logo_url:                     Option[String]        = None,
+  image_cover_url:              Option[String]        = None,
+  video_url:                    Option[String]        = None,
+  website_url:                  Option[String]        = None,
   is_auto_assigned_to_partners: Option[Boolean]       = None,
-  status:            Option[String]        = None,
-  siret:             Option[String]        = None,
-  tag:               Option[String]        = None,
-  custom:            Option[JsObject]      = None,
-  address:           Option[Address]       = None
+  status:                       Option[String]        = None,
+  siret:                        Option[String]        = None,
+  tag:                          Option[String]        = None,
+  custom:                       Option[JsObject]      = None,
+  address:                      Option[Address]       = None
 )
 
 object Enterprise {
