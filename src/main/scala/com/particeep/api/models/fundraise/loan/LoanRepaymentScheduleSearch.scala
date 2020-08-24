58adf6754fe8e6e58e1820417900d78e423ca1bc
quyanlin <qu.yanlin@particeep.com>
2020-08-24 10:20:43 +0200

    corrigé

--- src/main/scala/com/particeep/api/models/fundraise/loan/LoanRepaymentScheduleSearch.scala
+++ src/main/scala/com/particeep/api/models/fundraise/loan/LoanRepaymentScheduleSearch.scala
@@ -3,8 +3,8 @@ package com.particeep.api.models.fundraise.loan
 import java.time.ZonedDateTime
 
 case class LoanRepaymentScheduleSearch(
-  payment_data_after:  Option[ZonedDateTime] = None,
-  payment_data_before: Option[ZonedDateTime] = None,
+  payment_date_after:  Option[ZonedDateTime] = None,
+  payment_date_before: Option[ZonedDateTime] = None,
   user_id:             Option[String]        = None,
   enterprise_id:       Option[String]        = None,
   fundraise_id:        Option[String]        = None,
