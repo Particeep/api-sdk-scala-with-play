2435f7680c1ec2b4ef55fe957b0f0c3c3384d643
fhammache <faredj.hammache@particeep.com>
2021-03-10 14:05:17 +0100

    add description_financial

--- src/main/scala/com/particeep/api/models/fundraise/loan/FundraiseLoanCreation.scala
+++ src/main/scala/com/particeep/api/models/fundraise/loan/FundraiseLoanCreation.scala
@@ -1,31 +1,32 @@
 package com.particeep.api.models.fundraise.loan
 
-import java.time.ZonedDateTime
-
 import com.particeep.api.core.Formatter
 import com.particeep.api.models.enums.Currency.Currency
 import play.api.libs.json.{ JsObject, Json }
 
+import java.time.ZonedDateTime
+
 case class FundraiseLoanCreation(
-  name:                String,
-  description_short:   Option[String]        = None,
-  description_long:    Option[String]        = None,
-  description_offline: Option[String]        = None,
-  disclaimer_risk:     Option[String]        = None,
-  disclaimer_fees:     Option[String]        = None,
-  disclaimer_payment:  Option[String]        = None,
-  enterprise_id:       Option[String]        = None,
-  recipient_id:        Option[String]        = None,
-  recipient_type:      Option[String]        = None,
-  start_at:            Option[ZonedDateTime] = None,
-  end_at:              Option[ZonedDateTime] = None,
-  amount_target:       Long,
-  amount_target_max:   Option[Long]          = None,
-  currency:            Currency,
-  score:               Option[String]        = None,
-  tag:                 Option[String]        = None,
-  offer:               LoanOfferCreation,
-  custom:              Option[JsObject]      = None
+  name:                  String,
+  description_short:     Option[String]        = None,
+  description_long:      Option[String]        = None,
+  description_offline:   Option[String]        = None,
+  description_financial: Option[String]        = None,
+  disclaimer_risk:       Option[String]        = None,
+  disclaimer_fees:       Option[String]        = None,
+  disclaimer_payment:    Option[String]        = None,
+  enterprise_id:         Option[String]        = None,
+  recipient_id:          Option[String]        = None,
+  recipient_type:        Option[String]        = None,
+  start_at:              Option[ZonedDateTime] = None,
+  end_at:                Option[ZonedDateTime] = None,
+  amount_target:         Long,
+  amount_target_max:     Option[Long]          = None,
+  currency:              Currency,
+  score:                 Option[String]        = None,
+  tag:                   Option[String]        = None,
+  offer:                 LoanOfferCreation,
+  custom:                Option[JsObject]      = None
 )
 
 object FundraiseLoanCreation {
--- src/main/scala/com/particeep/api/models/fundraise/loan/FundraiseLoanRunningEdition.scala
+++ src/main/scala/com/particeep/api/models/fundraise/loan/FundraiseLoanRunningEdition.scala
@@ -1,27 +1,28 @@
 package com.particeep.api.models.fundraise.loan
 
-import java.time.ZonedDateTime
-
 import com.particeep.api.core.Formatter
 import play.api.libs.json.{ JsObject, Json }
+
+import java.time.ZonedDateTime
 
 /**
  * Created by Noe on 16/05/2017.
  */
 case class FundraiseLoanRunningEdition(
-  name:                Option[String]        = None,
-  description_short:   Option[String]        = None,
-  description_long:    Option[String]        = None,
-  description_offline: Option[String]        = None,
-  disclaimer_risk:     Option[String]        = None,
-  disclaimer_fees:     Option[String]        = None,
-  disclaimer_payment:  Option[String]        = None,
-  end_at:              Option[ZonedDateTime] = None,
-  score:               Option[String]        = None,
-  is_featured:         Option[Boolean]       = None,
-  form_id:             Option[String]        = None,
-  tag:                 Option[String]        = None,
-  custom:              Option[JsObject]      = None
+  name:                  Option[String]        = None,
+  description_short:     Option[String]        = None,
+  description_long:      Option[String]        = None,
+  description_offline:   Option[String]        = None,
+  description_financial: Option[String]        = None,
+  disclaimer_risk:       Option[String]        = None,
+  disclaimer_fees:       Option[String]        = None,
+  disclaimer_payment:    Option[String]        = None,
+  end_at:                Option[ZonedDateTime] = None,
+  score:                 Option[String]        = None,
+  is_featured:           Option[Boolean]       = None,
+  form_id:               Option[String]        = None,
+  tag:                   Option[String]        = None,
+  custom:                Option[JsObject]      = None
 )
 
 object FundraiseLoanRunningEdition {
