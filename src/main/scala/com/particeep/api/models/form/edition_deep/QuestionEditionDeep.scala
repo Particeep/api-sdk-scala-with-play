74d34e4bb5627fb32ffd4e9aea7321d286129edf
fhammache <faredj.hammache@particeep.com>
2021-08-04 16:06:23 +0200

    add valid_possibility_ids field

--- src/main/scala/com/particeep/api/models/form/edition_deep/QuestionEditionDeep.scala
+++ src/main/scala/com/particeep/api/models/form/edition_deep/QuestionEditionDeep.scala
@@ -5,15 +5,16 @@ import com.particeep.api.models.form.edition_deep
 import play.api.libs.json.Json
 
 case class QuestionEditionDeep(
-    id:                 Option[String],
-    label:              Option[Map[String, String]]                      = None,
-    description:        Option[Map[String, String]]                      = None,
-    possibility_id_dep: Option[String]                                   = None,
-    question_type:      Option[QuestionType],
-    required:           Option[Boolean],
-    index:              Option[Int],
-    pattern:            Option[String]                                   = None,
-    possibilities:      Option[Seq[edition_deep.PossibilityEditionDeep]]
+    id:                    Option[String],
+    label:                 Option[Map[String, String]]                      = None,
+    description:           Option[Map[String, String]]                      = None,
+    possibility_id_dep:    Option[String]                                   = None,
+    valid_possibility_ids: Option[String]                                   = None,
+    question_type:         Option[QuestionType],
+    required:              Option[Boolean],
+    index:                 Option[Int],
+    pattern:               Option[String]                                   = None,
+    possibilities:         Option[Seq[edition_deep.PossibilityEditionDeep]]
 )
 
 object QuestionEditionDeep {
