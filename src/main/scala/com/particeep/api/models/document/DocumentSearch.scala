ca4aa5eb47f60e15cdd6d30a3b96887bfe7e8df1
lasne_noe <noe.lasne@particeep.com>
2022-06-16 11:45:22 +0200

    add ids to document_search

--- src/main/scala/com/particeep/api/models/document/DocumentSearch.scala
+++ src/main/scala/com/particeep/api/models/document/DocumentSearch.scala
@@ -1,6 +1,7 @@
 package com.particeep.api.models.document
 
-import play.api.libs.json.Json
+import play.api.data.Form
+import play.api.data.Forms.{ mapping, optional, text }
 
 /**
  * Created by Noe on 04/01/2017.
@@ -16,9 +17,25 @@ case class DocumentSearch(
     external_id:  Option[String] = None,
     doc_type:     Option[String] = None,
     tag:          Option[String] = None,
-    custom:       Option[String] = None
+    custom:       Option[String] = None,
+    ids:          Option[String] = None
 )
 
 object DocumentSearch {
-  val format = Json.format[DocumentSearch]
+  val document_search_form = Form(
+    mapping(
+      "owner_id" -> optional(text),
+      "target_id" -> optional(text),
+      "target_type" -> optional(text),
+      "path" -> optional(text),
+      "name" -> optional(text),
+      "description" -> optional(text),
+      "content_type" -> optional(text),
+      "external_id" -> optional(text),
+      "doc_type" -> optional(text),
+      "tag" -> optional(text),
+      "custom" -> optional(text),
+      "ids" -> optional(text)
+    )(DocumentSearch.apply)(DocumentSearch.unapply)
+  )
 }
