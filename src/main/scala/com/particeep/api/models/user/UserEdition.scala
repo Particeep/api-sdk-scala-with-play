a06c5a40dcfc8e29fd33382ec6d48370cb97046b
Faredj HAMMACHE <43962658+fhammache@users.noreply.github.com>
2022-09-15 17:11:54 +0200

    Revert "add subject_to_income_tax"

--- src/main/scala/com/particeep/api/models/user/UserEdition.scala
+++ src/main/scala/com/particeep/api/models/user/UserEdition.scala
@@ -31,7 +31,6 @@ case class UserEdition(
     viadeo_url:                    Option[String]        = None,
     allow_mail_notifications:      Option[Boolean]       = None,
     does_pay_taxes:                Option[Boolean]       = None,
-    subject_to_income_tax:         Option[Boolean]       = None,
     single_fixed_tax_base_1:       Option[Boolean]       = None,
     single_fixed_tax_base_2_and_3: Option[Boolean]       = None,
     profession:                    Option[String]        = None,
