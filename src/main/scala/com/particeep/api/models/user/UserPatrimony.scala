28f790f5ed225524bb9dd94bd5b6ebb68698c213
Maxence PARFAIT <maxence.parfait@particeep.com>
2022-09-28 14:49:40 +0200

    remove compute method

--- src/main/scala/com/particeep/api/models/user/UserPatrimony.scala
+++ src/main/scala/com/particeep/api/models/user/UserPatrimony.scala
@@ -10,21 +10,7 @@ case class UserPatrimony(
     financial_patrimony: Option[Long],
     monthly_engagement:  Option[Long],
     exploit_result:      Option[Long]
-) {
-  private[this] def compute_net_patrimony: Long = {
-    val monthly_permanent_value = monthly_permanent.getOrElse(0L) * 12
-    val temporary_value = temporary.getOrElse(0L)
-    val monthly_financial_value = monthly_financial.getOrElse(0L) * 12
-    val financial_patrimony_value = financial_patrimony.getOrElse(0L)
-    val monthly_engagement_value = monthly_engagement.getOrElse(0L) * 12
-
-    monthly_permanent_value + temporary_value + monthly_financial_value + financial_patrimony_value - monthly_engagement_value
-  }
-
-  def net_patrimony: Long = {
-    exploit_result.getOrElse(compute_net_patrimony)
-  }
-}
+)
 
 object UserPatrimony {
   implicit val format = Jsonx.formatCaseClass[UserPatrimony]
