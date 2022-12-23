f77bf5a69b0aa1959bba7462265a9722c4178dee
Amin JELLAD <amin.jellad@particeep.com>
2022-12-23 17:53:06 +0100

    updated user patrimony

--- src/main/scala/com/particeep/api/models/user/UserPatrimony.scala
+++ src/main/scala/com/particeep/api/models/user/UserPatrimony.scala
@@ -4,10 +4,10 @@ import ai.x.play.json.Encoders._
 import ai.x.play.json.Jsonx
 
 case class UserPatrimony(
-    yearly_income:     Option[Long],
-    total_liquidity:   Option[Long],
-    yearly_engagement: Option[Long],
-    exploit_result:    Option[Long]
+    yearly_income:     Long = 0L,
+    total_liquidity:   Long = 0L,
+    yearly_engagement: Long = 0L,
+    exploit_result:    Long = 0L
 )
 
 object UserPatrimony {
