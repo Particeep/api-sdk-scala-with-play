03931febeb0732a41cae88c68e64f6a72a851b42
fhammache <faredj.hammache@particeep.com>
2023-01-05 11:49:08 +0100

    more pro qualifications items

--- src/main/scala/com/particeep/api/models/enums/ProQualification.scala
+++ src/main/scala/com/particeep/api/models/enums/ProQualification.scala
@@ -5,8 +5,10 @@ sealed trait ProQualification extends Product with Serializable with Enum
 object ProQualification extends EnumHelper[ProQualification] {
 
   case object PROFESSIONAL extends ProQualification
+  case object NOT_PROFESSIONAL extends ProQualification
   case object SOPHISTICATED extends ProQualification
   case object NOT_SOPHISTICATED extends ProQualification
+  case object ELIGIBLE_COUNTERPARTY extends ProQualification
 
-  def values: Set[ProQualification] = Set(PROFESSIONAL, SOPHISTICATED, NOT_SOPHISTICATED)
+  def values: Set[ProQualification] = Set(PROFESSIONAL, NOT_PROFESSIONAL, SOPHISTICATED, NOT_SOPHISTICATED, ELIGIBLE_COUNTERPARTY)
 }
