fba0130efcae5debb799ca7eaceb237a8d5eaefe
lasne_noe <noe.lasne@particeep.com>
2023-01-17 17:02:35 +0100

    User tag to List

--- src/main/scala/com/particeep/api/models/user/UserData.scala
+++ src/main/scala/com/particeep/api/models/user/UserData.scala
@@ -51,7 +51,7 @@ case class UserData(
     partner_manager_id:            Option[String]           = None,
     partner_manager_company_business_name: Option[String]           = None,
     investor_score:                        Option[Long]             = None,
-    tag:                                   Option[String]           = None,
+    tag:                                   List[String]             = List(),
     pro_qualification:                     Option[String]           = None,
     account_validation_status:             Option[String]           = None,
     lang:                                  Option[String]           = None,
