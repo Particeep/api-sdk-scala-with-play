c9edaaf07c1587c2540409b49c38d4e9231bb612
Amin JELLAD <amin.jellad@particeep.com>
2023-08-16 14:42:45 +0200

    long to double

--- src/main/scala/com/particeep/api/models/user/UserPatrimonyEdition.scala
+++ src/main/scala/com/particeep/api/models/user/UserPatrimonyEdition.scala
@@ -4,10 +4,10 @@ import ai.x.play.json.Encoders._
 import ai.x.play.json.Jsonx
 
 case class UserPatrimonyEdition(
-    yearly_income:     Option[Long],
-    total_liquidity:   Option[Long],
-    yearly_engagement: Option[Long],
-    exploit_result:    Option[Long]
+    yearly_income:     Option[Double],
+    total_liquidity:   Option[Double],
+    yearly_engagement: Option[Double],
+    exploit_result:    Option[Double]
 )
 
 object UserPatrimonyEdition {
