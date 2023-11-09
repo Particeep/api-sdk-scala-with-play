4eaf6ac6cef3c846c1fdc539786310cceb4d28d2
Maxence PARFAIT <maxence.parfait@particeep.com>
2023-11-09 17:12:36 +0100

    add offline_payment_method fields

--- src/main/scala/com/particeep/api/models/fundraise/equity/InvestmentOption.scala
+++ src/main/scala/com/particeep/api/models/fundraise/equity/InvestmentOption.scala
@@ -1,19 +1,21 @@
 package com.particeep.api.models.fundraise.equity
 
+import com.particeep.api.models.enums.PaymentMethod.OfflinePaymentMethod
 import play.api.libs.json.{ JsArray, JsObject, Json }
 
 case class InvestmentOption(
-    amount:                 Option[Int]      = None,
-    partner_fees:           Option[Int]      = None,
-    unicia_id:              Option[String]   = None,
-    comment:                Option[String]   = None,
-    private_comment:        Option[String]   = None,
-    tag:                    Option[String]   = None,
-    co_issuers:             Option[JsArray]  = None,
-    rib:                    Option[String]   = None,
-    dismemberment_duration: Option[Int]      = None,
-    dismemberment_rate:     Option[Double]   = None,
-    custom:                 Option[JsObject] = None
+    amount:                 Option[Int]                  = None,
+    partner_fees:           Option[Int]                  = None,
+    unicia_id:              Option[String]               = None,
+    comment:                Option[String]               = None,
+    private_comment:        Option[String]               = None,
+    tag:                    Option[String]               = None,
+    co_issuers:             Option[JsArray]              = None,
+    rib:                    Option[String]               = None,
+    dismemberment_duration: Option[Int]                  = None,
+    dismemberment_rate:     Option[Double]               = None,
+    custom:                 Option[JsObject]             = None,
+    offline_payment_method: Option[OfflinePaymentMethod] = None
 )
 
 object InvestmentOption {
