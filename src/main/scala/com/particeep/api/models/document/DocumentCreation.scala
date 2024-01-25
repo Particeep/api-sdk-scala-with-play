de967c850cc600d3e9902e581b1ed0099cf92143
Amin JELLAD <amin.jellad@particeep.com>
2024-01-25 17:51:17 +0100

    added field scope on class Document, DocumentCreation and DocumentEdition

--- src/main/scala/com/particeep/api/models/document/DocumentCreation.scala
+++ src/main/scala/com/particeep/api/models/document/DocumentCreation.scala
@@ -13,6 +13,7 @@ case class DocumentCreation(
     path:                   Option[String]   = None,
     locked:                 Option[Boolean]  = None,
     override_existing_file: Option[Boolean]  = None,
+    scope:                  Option[String]   = None,
     tag:                    Option[String]   = None,
     custom:                 Option[JsObject] = None
 )
