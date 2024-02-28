8b2493932c228d60585fcef4f43d46461a4d73b8
Louis Marsais <louis.marsais@particeep.com>
2024-02-28 16:58:00 +0100

    change following compute derived data on api

--- src/main/scala/com/particeep/api/models/kyc_ecsp/KycEcsp.scala
+++ src/main/scala/com/particeep/api/models/kyc_ecsp/KycEcsp.scala
@@ -18,6 +18,8 @@ sealed trait KycEcsp {
   def user_id: String
   def updated_at: Option[OffsetDateTime]
   def validated_at: Option[OffsetDateTime]
+  def isSophisticated: Boolean
+  def investmentAdviceThreashold: Long
   def tag: Option[String]
   def custom: Option[JsObject]
 }
@@ -35,22 +37,26 @@ object KycEcsp {
       annual_income:              Option[Long]           = None,
       amount_of_assets:           Option[Long]           = None,
       amount_of_debt:             Option[Long]           = None,
+      isSophisticated:            Boolean                = false,
+      investmentAdviceThreashold: Long                   = 0L,
       tag:                        Option[String]         = None,
       custom:                     Option[JsObject]       = None
   ) extends KycEcsp
 
   final case class Legal(
-      id:                     String,
-      created_at:             OffsetDateTime,
-      user_id:                String,
-      updated_at:             Option[OffsetDateTime] = None,
-      validated_at:           Option[OffsetDateTime] = None,
-      has_high_capital:       Boolean                = false, // FR : Capital de l'entreprise
-      has_high_sales_figures: Boolean                = false, // FR : Chiffre d'affaire
-      has_high_balance_sheet: Boolean                = false, // FR : Bilan comptable
-      operating_income:       Option[Long]           = None,
-      tag:                    Option[String]         = None,
-      custom:                 Option[JsObject]       = None
+      id:                         String,
+      created_at:                 OffsetDateTime,
+      user_id:                    String,
+      updated_at:                 Option[OffsetDateTime] = None,
+      validated_at:               Option[OffsetDateTime] = None,
+      has_high_capital:           Boolean                = false, // FR : Capital de l'entreprise
+      has_high_sales_figures:     Boolean                = false, // FR : Chiffre d'affaire
+      has_high_balance_sheet:     Boolean                = false, // FR : Bilan comptable
+      operating_income:           Option[Long]           = None,
+      isSophisticated:            Boolean                = false,
+      investmentAdviceThreashold: Long                   = 0L,
+      tag:                        Option[String]         = None,
+      custom:                     Option[JsObject]       = None
   ) extends KycEcsp
 
   implicit val kyc_ecsp_natural_format = Jsonx.formatCaseClassUseDefaults[KycEcsp.Natural]
