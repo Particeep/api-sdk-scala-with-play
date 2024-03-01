4b279bcac673904f3ebb3a59e4f22c2a93a0dd40
Louis Marsais <louis.marsais@particeep.com>
2024-03-01 16:11:49 +0100

    following api MR

--- src/main/scala/com/particeep/api/models/kyc_ecsp/KycEcsp.scala
+++ src/main/scala/com/particeep/api/models/kyc_ecsp/KycEcsp.scala
@@ -6,10 +6,10 @@ import ai.x.play.json.Jsonx
 import play.api.libs.json.JsObject
 
 /*
- * KYC ECSP stand for : Know Your Consumer, European Crowdfunding Service Providers
+ * KYC ECSP stands for : Know Your Consumer, European Crowdfunding Service Providers
  * in French : PSFP, Prestataire de Services de Financement Participatif
  *
- * Here are handeled features for our consumers doing crowdfunding type of fundraises and are bounded to european rules
+ * Here are handled features for our consumers doing crowdfunding type of fundraises and are bounded to european rules
  */
 
 sealed trait KycEcsp {
@@ -18,45 +18,45 @@ sealed trait KycEcsp {
   def user_id: String
   def updated_at: Option[OffsetDateTime]
   def validated_at: Option[OffsetDateTime]
-  def isSophisticated: Boolean
-  def investmentAdviceThreashold: Long
+  def is_sophisticated: Boolean
+  def investment_advice_threshold: Long
   def tag: Option[String]
   def custom: Option[JsObject]
 }
 
 object KycEcsp {
   final case class Natural(
-      id:                         String,
-      created_at:                 OffsetDateTime,
-      user_id:                    String,
-      updated_at:                 Option[OffsetDateTime] = None,
-      validated_at:               Option[OffsetDateTime] = None,
-      is_pro:                     Boolean                = false,
-      has_high_income:            Boolean                = false,
-      has_done_many_transactions: Boolean                = false,
-      annual_income:              Option[Long]           = None,
-      amount_of_assets:           Option[Long]           = None,
-      amount_of_debt:             Option[Long]           = None,
-      isSophisticated:            Boolean                = false,
-      investmentAdviceThreashold: Long                   = 0L,
-      tag:                        Option[String]         = None,
-      custom:                     Option[JsObject]       = None
+      id:                          String,
+      created_at:                  OffsetDateTime,
+      user_id:                     String,
+      updated_at:                  Option[OffsetDateTime] = None,
+      validated_at:                Option[OffsetDateTime] = None,
+      is_pro:                      Boolean                = false,
+      has_high_income:             Boolean                = false,
+      has_done_many_transactions:  Boolean                = false,
+      annual_income:               Option[Long]           = None,
+      amount_of_assets:            Option[Long]           = None,
+      amount_of_debt:              Option[Long]           = None,
+      is_sophisticated:            Boolean                = false,
+      investment_advice_threshold: Long                   = 0L,
+      tag:                         Option[String]         = None,
+      custom:                      Option[JsObject]       = None
   ) extends KycEcsp
 
   final case class Legal(
-      id:                         String,
-      created_at:                 OffsetDateTime,
-      user_id:                    String,
-      updated_at:                 Option[OffsetDateTime] = None,
-      validated_at:               Option[OffsetDateTime] = None,
-      has_high_capital:           Boolean                = false, // FR : Capital de l'entreprise
-      has_high_sales_figures:     Boolean                = false, // FR : Chiffre d'affaire
-      has_high_balance_sheet:     Boolean                = false, // FR : Bilan comptable
-      operating_income:           Option[Long]           = None,
-      isSophisticated:            Boolean                = false,
-      investmentAdviceThreashold: Long                   = 0L,
-      tag:                        Option[String]         = None,
-      custom:                     Option[JsObject]       = None
+      id:                          String,
+      created_at:                  OffsetDateTime,
+      user_id:                     String,
+      updated_at:                  Option[OffsetDateTime] = None,
+      validated_at:                Option[OffsetDateTime] = None,
+      has_high_capital:            Boolean                = false, // FR : Capital de l'entreprise
+      has_high_sales_figures:      Boolean                = false, // FR : Chiffre d'affaire
+      has_high_balance_sheet:      Boolean                = false, // FR : Bilan comptable
+      operating_income:            Option[Long]           = None,
+      is_sophisticated:            Boolean                = false,
+      investment_advice_threshold: Long                   = 0L,
+      tag:                         Option[String]         = None,
+      custom:                      Option[JsObject]       = None
   ) extends KycEcsp
 
   implicit val kyc_ecsp_natural_format = Jsonx.formatCaseClassUseDefaults[KycEcsp.Natural]
