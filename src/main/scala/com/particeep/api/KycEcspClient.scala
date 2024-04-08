1b6e98e68cf208cee8e87999491fc344c209663b
Louis Marsais <louis.marsais@particeep.com>
2024-04-08 15:54:05 +0200

    usining implicit formatter and no default value

--- src/main/scala/com/particeep/api/KycEcspClient.scala
+++ src/main/scala/com/particeep/api/KycEcspClient.scala
@@ -7,7 +7,6 @@ import play.api.libs.json.{ JsNull, Json }
 import com.particeep.api.core._
 import com.particeep.api.models.ErrorResult
 import com.particeep.api.models.enums.KycEcspType
-import com.particeep.api.models.kyc_ecsp.KycEcspParser
 
 trait KycEcspCapability {
   self: WSClient =>
@@ -29,19 +28,19 @@ class KycEcspClient(val ws: WSClient, val credentials: Option[ApiCredential] = N
   import KycEcspClient._
 
   def findCurrentKyc(user_id: String, kyc_type: KycEcspType, timeout: Long = defaultTimeOut)(implicit ec: ExecutionContext): Future[Either[ErrorResult, KycEcsp]] = {
-    ws.get[KycEcsp](s"$endPoint/$user_id/$kyc_type", timeout)(ec, creds, KycEcspParser.format(kyc_type))
+    ws.get[KycEcsp](s"$endPoint/$user_id/$kyc_type", timeout)
   }
 
   def create(user_id: String, kyc: KycEcspUpdate, kyc_type: KycEcspType, timeout: Long = defaultTimeOut)(implicit ec: ExecutionContext): Future[Either[ErrorResult, KycEcsp]] = {
-    ws.put[KycEcsp](s"$endPoint/$user_id/$kyc_type", timeout, Json.toJson(kyc))(ec, creds, KycEcspParser.format(kyc_type))
+    ws.put[KycEcsp](s"$endPoint/$user_id/$kyc_type", timeout, Json.toJson(kyc))
   }
 
   def update(user_id: String, kyc: KycEcspUpdate, kyc_type: KycEcspType, timeout: Long = defaultTimeOut)(implicit ec: ExecutionContext): Future[Either[ErrorResult, KycEcsp]] = {
-    ws.post[KycEcsp](s"$endPoint/$user_id/$kyc_type", timeout, Json.toJson(kyc))(ec, creds, KycEcspParser.format(kyc_type))
+    ws.post[KycEcsp](s"$endPoint/$user_id/$kyc_type", timeout, Json.toJson(kyc))
   }
 
   def validate(user_id: String, kyc_type: KycEcspType, timeout: Long = defaultTimeOut)(implicit ec: ExecutionContext): Future[Either[ErrorResult, KycEcsp]] = {
-    ws.post[KycEcsp](s"$endPoint/$user_id/$kyc_type/validate", timeout, JsNull)(ec, creds, KycEcspParser.format(kyc_type))
+    ws.post[KycEcsp](s"$endPoint/$user_id/$kyc_type/validate", timeout, JsNull)
   }
 
   def delete(user_id: String, timeout: Long = defaultTimeOut)(implicit ec: ExecutionContext): Future[Either[ErrorResult, KycEcspCollection]] = {
--- src/main/scala/com/particeep/api/models/kyc_ecsp/KycEcspCollection.scala
+++ src/main/scala/com/particeep/api/models/kyc_ecsp/KycEcspCollection.scala
@@ -3,7 +3,6 @@ package com.particeep.api.models.kyc_ecsp
 import ai.x.play.json.Encoders.encoder
 import ai.x.play.json.Jsonx
 import play.api.libs.json.OFormat
-import KycEcspParser.{ kyc_ecsp_legal_format, kyc_ecsp_natural_format }
 
 case class KycEcspCollection(
     kycs_legal:   Seq[KycEcsp.Legal]   = Seq(),
