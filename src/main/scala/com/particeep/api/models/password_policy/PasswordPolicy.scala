9653bd415c4f8fd8c66a896a83605a9fe2e703fb
Amin JELLAD <amin.jellad@particeep.com>
2024-05-17 15:01:53 +0200

    simplified type for PasswordPolicy

--- src/main/scala/com/particeep/api/models/password_policy/PasswordPolicy.scala
+++ src/main/scala/com/particeep/api/models/password_policy/PasswordPolicy.scala
@@ -1,11 +1,8 @@
 package com.particeep.api.models.password_policy
 
-import play.api.libs.json.{ Format, Json, Writes }
+import play.api.libs.json.{ Format, Json }
 
-import java.time.{ Duration, OffsetDateTime }
-import scala.util.matching.Regex
-
-import com.particeep.api.core.Formatter.regex_format
+import java.time.OffsetDateTime
 
 final case class PasswordPolicy(
   id:                            String,
@@ -13,29 +10,13 @@ final case class PasswordPolicy(
   created_by:                    String,
   updated_at:                    OffsetDateTime,
   password_minimum_length:       Int,
-  password_regex:                Regex,
-  password_expiration_frequency: Duration,
+  password_regex:                String,
+  password_expiration_frequency: Int,
   password_reuse_limit:          Int,
-  password_reuse_period:         Duration,
+  password_reuse_period:         Int,
   login_retry_limit:             Int
 )
 
 object PasswordPolicy {
-
-  private[this] val write_pp: Writes[PasswordPolicy] = (pp: PasswordPolicy) => {
-    Json.obj(
-      "id"                            -> pp.id,
-      "created_at"                    -> pp.created_at,
-      "created_by"                    -> pp.created_by,
-      "updated_at"                    -> pp.updated_at,
-      "password_minimum_length"       -> pp.password_minimum_length,
-      "password_regex"                -> pp.password_regex.toString(),
-      "password_expiration_frequency" -> pp.password_expiration_frequency.toDays,
-      "password_reuse_limit"          -> pp.password_reuse_limit,
-      "password_reuse_period"         -> pp.password_reuse_period.toSeconds,
-      "login_retry_limit"             -> pp.login_retry_limit
-    )
-  }
-
-  implicit val pp_format: Format[PasswordPolicy] = Format(Json.reads[PasswordPolicy], write_pp)
+  implicit val pp_format: Format[PasswordPolicy] = Json.format[PasswordPolicy]
 }
