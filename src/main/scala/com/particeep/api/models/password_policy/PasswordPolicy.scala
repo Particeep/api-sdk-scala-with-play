542a017bdb8db84c596c4abe4c460b0459927d99
Amin JELLAD <amin.jellad@particeep.com>
2024-05-21 14:20:06 +0200

    fixed typo

--- src/main/scala/com/particeep/api/models/password_policy/PasswordPolicy.scala
+++ src/main/scala/com/particeep/api/models/password_policy/PasswordPolicy.scala
@@ -11,7 +11,7 @@ final case class PasswordPolicy(
   updated_at:                    OffsetDateTime,
   password_minimum_length:       Long,
   password_regex:                String,
-  password_expiration_frequency: Long, // in days
+  password_expiration_frequency: Long, // in seconds
   password_reuse_limit:          Long,
   password_reuse_period:         Long, // in seconds
   login_retry_limit:             Long
