74a2424a13636d6ceddd03181704ce09c6a1ee56
Louis Marsais <louis.marsais@particeep.com>
2024-06-21 14:58:24 +0200

    MR comment #1

--- src/main/scala/com/particeep/api/models/control/ControlUpdate.scala
+++ src/main/scala/com/particeep/api/models/control/ControlUpdate.scala
@@ -3,8 +3,8 @@ package com.particeep.api.models.control
 import play.api.libs.json.{ Json, OFormat }
 
 final case class ControlUpdate(
-  comment:         Option[String]      = None,
-  document_id_seq: Option[Seq[String]] = None
+  comment:      Option[String]       = None,
+  document_ids: Option[List[String]] = None
 )
 
 object ControlUpdate {
