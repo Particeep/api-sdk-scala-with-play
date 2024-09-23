35c82d1183e449aa8ba02cbb76609b425f31ea4d
Aglae Tournois <aglae.tournois@particeep.com>
2024-09-23 17:05:45 +0200

    refactor(TransactionWalletStatus): Change format of TransactionWalletStatus Enum

--- src/main/scala/com/particeep/api/models/wallet/TransactionWalletData.scala
+++ src/main/scala/com/particeep/api/models/wallet/TransactionWalletData.scala
@@ -7,8 +7,7 @@ import play.api.libs.json.{ OFormat, Writes }
 import java.time.OffsetDateTime
 
 import com.particeep.api.core.Formatter
-import com.particeep.api.models.enums.TransactionWalletStatus.TransactionWalletStatus
-import com.particeep.api.models.enums.{ Currency, TransactionWalletOperation }
+import com.particeep.api.models.enums.{ Currency, TransactionWalletOperation, TransactionWalletStatus }
 
 case class TransactionWalletData(
   id:                 String,
