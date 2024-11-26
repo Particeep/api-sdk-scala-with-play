829afa3d158ab74215cffbd0d1833dc06ef76dea
fhammache <faredj.hammache@particeep.com>
2024-11-26 14:47:40 +0100

    update signer type enum

--- src/main/scala/com/particeep/api/models/enums/SignerType.scala
+++ src/main/scala/com/particeep/api/models/enums/SignerType.scala
@@ -4,11 +4,25 @@ sealed trait SignerType extends Product with Serializable with Enum
 
 object SignerType extends EnumHelper[SignerType] {
 
-  case object CONJOINT     extends SignerType
-  case object INVESTOR     extends SignerType
-  case object PARTNER      extends SignerType
-  case object USUFRUCTARY  extends SignerType
-  case object ENTREPRENEUR extends SignerType
+  case object CONJOINT             extends SignerType
+  case object INVESTOR             extends SignerType
+  case object PARTNER              extends SignerType
+  case object USUFRUCTARY          extends SignerType
+  case object ENTREPRENEUR         extends SignerType
+  case object SPOUSE               extends SignerType
+  case object GUARDIAN             extends SignerType
+  case object CURATOR              extends SignerType
+  case object LEGAL_REPRESENTATIVE extends SignerType
 
-  val values: Set[SignerType] = Set(CONJOINT, INVESTOR, PARTNER, USUFRUCTARY, ENTREPRENEUR)
+  val values: Set[SignerType] = Set(
+    CONJOINT,
+    INVESTOR,
+    PARTNER,
+    USUFRUCTARY,
+    ENTREPRENEUR,
+    SPOUSE,
+    GUARDIAN,
+    CURATOR,
+    LEGAL_REPRESENTATIVE
+  )
 }
