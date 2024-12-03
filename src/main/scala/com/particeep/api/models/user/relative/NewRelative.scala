ad7b96096ac99a63a771fc7a5e7d59821374226c
fhammache <faredj.hammache@particeep.com>
2024-12-03 11:02:25 +0100

    update

--- src/main/scala/com/particeep/api/models/user/relative/NewRelative.scala
+++ src/main/scala/com/particeep/api/models/user/relative/NewRelative.scala
@@ -1,6 +1,7 @@
 package com.particeep.api.models.user.relative
 
-import play.api.libs.json.{ Json, OFormat }
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 import com.particeep.api.models.user.User
 
@@ -10,6 +11,20 @@ case class NewRelative(
 )
 
 object NewRelative {
-  private[this] implicit val user_format: OFormat[User] = User.format
-  val format: OFormat[NewRelative]                      = Json.format[NewRelative]
+  private[this] implicit val user_writes: OWrites[User] = User.format
+  private[this] val user_reads: Reads[User]             = User.format
+
+  private[this] implicit val reads: Reads[NewRelative] = (
+    user_reads and
+      (__ \ "role").read[RelativeRole]
+  ) { (user, role) =>
+    NewRelative(
+      user = user,
+      role = role
+    )
+  }
+
+  private[this] implicit val writes: Writes[NewRelative] = Json.writes[NewRelative]
+
+  val format: Format[NewRelative] = Format(reads, writes)
 }
