4dcda66c61f228cc3e5f0f914aaea48f92bb5b66
Louis Marsais <louis.marsais@particeep.com>
2025-03-04 11:51:47 +0100

    Done

---
+++ src/main/scala/com/particeep/api/models/signature/CustomSignerPosition.scala
@@ -0,0 +1,26 @@
+package com.particeep.api.models.signature
+
+import ai.x.play.json.Encoders.encoder
+import ai.x.play.json.Jsonx
+import play.api.libs.json.OFormat
+
+import com.particeep.api.models.enums.SignerType
+
+final case class CustomSignerPosition(
+                                       signer_type: SignerType,
+                                       configs:     List[CustomPosition]
+                                     )
+
+object CustomSignerPosition {
+  implicit val CustomSignerPositionFormat: OFormat[CustomSignerPosition] = Jsonx.formatCaseClass[CustomSignerPosition]
+}
+
+final case class CustomPosition(
+                                 page:  Int,
+                                 x:     Int,
+                                 y:     Int,
+                                 width: Int = 180 // Only for yousign for now.
+                               )
+object CustomPosition {
+  implicit val CustomPositionFormat: OFormat[CustomPosition] = Jsonx.formatCaseClass[CustomPosition]
+}
