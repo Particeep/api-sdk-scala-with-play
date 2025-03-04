8bb0c0c3c09f9ecacb6f0711aabbb2bbb52ecbd9
Louis Marsais <louis.marsais@particeep.com>
2025-03-04 11:51:47 +0100

    Done

---
+++ src/main/scala/com/particeep/api/models/signature/SignaturePosition.scala
@@ -0,0 +1,19 @@
+package com.particeep.api.models.signature
+
+import ai.x.play.json.Encoders.encoder
+import ai.x.play.json.Jsonx
+import play.api.libs.json.OFormat
+
+import java.time.OffsetDateTime
+
+final case class SignaturePosition(
+                                    id:          String,
+                                    created_at:  OffsetDateTime       ,
+                                    document_id: String,
+                                    positions:   List[CustomSignerPosition],
+                                  )
+
+object SignaturePosition {
+  implicit val SignaturePositionFormat: OFormat[SignaturePosition] = Jsonx.formatCaseClass[SignaturePosition]
+}
+
