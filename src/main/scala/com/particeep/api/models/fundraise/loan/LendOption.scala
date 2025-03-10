f8b9de6aa147726f02e35b2b224a266f50a2d47e
fhammache <faredj.hammache@particeep.com>
2025-03-10 21:02:12 +0100

    transaction co issuers

--- src/main/scala/com/particeep/api/models/fundraise/loan/LendOption.scala
+++ src/main/scala/com/particeep/api/models/fundraise/loan/LendOption.scala
@@ -3,19 +3,19 @@ package com.particeep.api.models.fundraise.loan
 import play.api.libs.json.{ JsArray, JsObject, Json, OFormat }
 
 import com.particeep.api.models.enums.PaymentMethod.OfflinePaymentMethod
-import com.particeep.api.models.user.relative.RelativeMetaData
+import com.particeep.api.models.transaction.CoIssuer
 
 case class LendOption(
-  amount:                 Option[Int]                    = None,
-  partner_fees:           Option[Int]                    = None,
-  unicia_id:              Option[String]                 = None,
-  comment:                Option[String]                 = None,
-  private_comment:        Option[String]                 = None,
-  tag:                    Option[String]                 = None,
-  co_issuers:             Option[JsArray]                = None,
-  new_co_issuers:         Option[List[RelativeMetaData]] = None,
-  custom:                 Option[JsObject]               = None,
-  offline_payment_method: Option[OfflinePaymentMethod]   = None
+  amount:                 Option[Int]                  = None,
+  partner_fees:           Option[Int]                  = None,
+  unicia_id:              Option[String]               = None,
+  comment:                Option[String]               = None,
+  private_comment:        Option[String]               = None,
+  tag:                    Option[String]               = None,
+  co_issuers:             Option[JsArray]              = None,
+  new_co_issuers:         Option[List[CoIssuer]]       = None,
+  custom:                 Option[JsObject]             = None,
+  offline_payment_method: Option[OfflinePaymentMethod] = None
 )
 
 object LendOption {
