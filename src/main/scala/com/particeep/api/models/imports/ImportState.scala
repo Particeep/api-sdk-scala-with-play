ab129fa1b9147f97bb339d3460438ecd9350953d
lasne_noe <noe.lasne@particeep.com>
2025-03-17 15:35:47 +0100

    import changes

---
+++ src/main/scala/com/particeep/api/models/imports/ImportState.scala
@@ -0,0 +1,24 @@
+package com.particeep.api.models.imports
+
+import play.api.libs.json.{JsObject, Json, OFormat}
+
+import java.time.OffsetDateTime
+
+import com.particeep.api.models.enums.{ImportStateStatus, ImportType}
+
+case class ImportState (
+                         id: String,
+                         created_at: OffsetDateTime,
+                         updated_at: Option[OffsetDateTime],
+                         status: ImportStateStatus,
+                         import_type: ImportType,
+                         line_processed: Int,
+                         nb_created: Int,
+                         nb_fail: Int,
+                         tag: Option[String] = None,
+                         custom: Option[JsObject] = None
+                       )
+
+object ImportState {
+  val format: OFormat[ImportState] = Json.format[ImportState]
+}
