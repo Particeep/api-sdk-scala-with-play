1cf854e899db2569098a5fb623abb439af6b696a
aymane_E <aymane.elmouhtarim@particeep.com>
2025-04-25 14:32:18 +0200

    minor

--- src/main/scala/com/particeep/api/models/enums/NewSignerType.scala
+++ src/main/scala/com/particeep/api/models/enums/NewSignerType.scala
@@ -1,9 +1,11 @@
 package com.particeep.api.models.enums
 
+import com.particeep.api.models.user.relative.RelativeRole
+
 sealed trait NewSignerType extends Product with Serializable with Enum
+// to come USUFRUCTUARY_LEGAL, _NATURAL, CO_USU
 
 object NewSignerType extends EnumHelper[NewSignerType] {
-
   case object INVESTOR_LEGAL       extends NewSignerType
   case object INVESTOR_NATURAL     extends NewSignerType
   case object CO_INVESTOR          extends NewSignerType
@@ -29,4 +31,35 @@ object NewSignerType extends EnumHelper[NewSignerType] {
     EXECUTIVE,
     BENEFICIARY
   )
+
+  def toOld(newSignerType: NewSignerType): SignerType = newSignerType match {
+    case INVESTOR_LEGAL | INVESTOR_NATURAL => SignerType.INVESTOR
+    case CO_INVESTOR                       => SignerType.CONJOINT
+    case PARTNER                           => SignerType.PARTNER
+    case USUFRUCTUARY                      => SignerType.USUFRUCTARY
+    case SPOUSE                            => SignerType.SPOUSE
+    case GUARDIAN                          => SignerType.GUARDIAN
+    case CURATOR                           => SignerType.CURATOR
+    case LEGAL_REPRESENTATIVE              => SignerType.LEGAL_REPRESENTATIVE
+    case EXECUTIVE                         => SignerType.EXECUTIVE
+    case BENEFICIARY                       => SignerType.BENEFICIARY
+  }
+
+  def fromInvestorType(investorType: InvestorType): NewSignerType = investorType match {
+    case InvestorType.LEGAL   => INVESTOR_LEGAL
+    case InvestorType.NATURAL => INVESTOR_NATURAL
+  }
+
+  def fromRelativeRole(relativeRole: RelativeRole): NewSignerType = relativeRole match {
+    case RelativeRole.SPOUSE               => SPOUSE
+    case RelativeRole.GUARDIAN             => GUARDIAN
+    case RelativeRole.CURATOR              => CURATOR
+    case RelativeRole.LEGAL_REPRESENTATIVE => LEGAL_REPRESENTATIVE
+  }
+
+  def fromOrganizationRole(organizationRole: OrganizationRole): NewSignerType = organizationRole match {
+    case OrganizationRole.EXECUTIVE            => EXECUTIVE
+    case OrganizationRole.BENEFICIARY          => BENEFICIARY
+    case OrganizationRole.LEGAL_REPRESENTATIVE => LEGAL_REPRESENTATIVE
+  }
 }
