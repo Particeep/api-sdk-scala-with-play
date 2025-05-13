be98968f04f140bb663ecbf05beb481689df3847
aymane_E <aymane.elmouhtarim@particeep.com>
2025-05-13 16:10:31 +0200

    minor

--- src/main/scala/com/particeep/api/models/enums/SignerType.scala
+++ src/main/scala/com/particeep/api/models/enums/SignerType.scala
@@ -1,5 +1,7 @@
 package com.particeep.api.models.enums
 
+import com.particeep.api.models.user.relative.RelativeRole
+
 sealed abstract class SignerType extends Product with Serializable with NewEnum
 // to come USUFRUCTUARY_LEGAL, _NATURAL CO_USU
 
@@ -29,4 +31,25 @@ object SignerType extends NewEnumSlickHelper[SignerType] {
     EXECUTIVE,
     BENEFICIARY
   )
+
+  def fromInvestorType(investorType: Option[InvestorType]): Option[SignerType] = investorType match {
+    case Some(InvestorType.LEGAL)   => Some(SignerType.INVESTOR_LEGAL)
+    case Some(InvestorType.NATURAL) => Some(SignerType.INVESTOR_NATURAL)
+    case None                       => None
+  }
+
+  def fromRelativeRole(relativeRole: Option[RelativeRole]): Option[SignerType] = relativeRole match {
+    case Some(RelativeRole.SPOUSE)               => Some(SignerType.SPOUSE)
+    case Some(RelativeRole.GUARDIAN)             => Some(SignerType.GUARDIAN)
+    case Some(RelativeRole.CURATOR)              => Some(SignerType.CURATOR)
+    case Some(RelativeRole.LEGAL_REPRESENTATIVE) => Some(SignerType.LEGAL_REPRESENTATIVE)
+    case None                                    => None
+  }
+
+  def fromOrganizationRole(organizationRole: Option[OrganizationRole]): Option[SignerType] = organizationRole match {
+    case Some(OrganizationRole.EXECUTIVE)            => Some(SignerType.EXECUTIVE)
+    case Some(OrganizationRole.BENEFICIARY)          => Some(SignerType.BENEFICIARY)
+    case Some(OrganizationRole.LEGAL_REPRESENTATIVE) => Some(SignerType.LEGAL_REPRESENTATIVE)
+    case None                                        => None
+  }
 }
